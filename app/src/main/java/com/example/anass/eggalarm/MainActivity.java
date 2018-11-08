e88be09a9a93b0ae9e5c79c4c8385befb5147a08
Tepenox <anasselafyabusiness@gmail.com>
2018-11-08 22:19:00 +0100

    fixed the ui adn added the pause fucntionality

--- app/src/main/java/com/example/anass/eggalarm/MainActivity.java
+++ app/src/main/java/com/example/anass/eggalarm/MainActivity.java
@@ -1,18 +1,13 @@
 package com.example.anass.eggalarm;
 
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import java.time.Duration;
-import java.time.LocalTime;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,9 +15,35 @@ public class MainActivity extends AppCompatActivity {
     TextView timerTextView;
     Button timerButton;
     CountDownTimer timer;
+    MediaPlayer mp;
+    int timeLeft;
+    // 0 stopped  1 active 2 paused 3 done
+    int timerState = 0;
+
 
     public void onButtonClick(View view) {
-        timer.start();
+        if (timerState == 0 || timerState == 2) {
+            timer.start();
+            timerButton.setText("pause");
+            timerState = 1;
+            timerSeekBar.setEnabled(false);
+        } else if (timerState == 1 || timerState == 3) {
+            if (timerState != 3) {
+                //pausing the timer here
+                timer.cancel();
+                setTimer(timeLeft);
+                timerButton.setText("resume");
+                timerState = 2;
+            } else {
+                timer.cancel();
+                mp.stop();
+                timerSeekBar.setEnabled(true);
+                timerState = 0;
+                timerSeekBar.setProgress(30);
+
+            }
+
+        }
 
     }
 
@@ -33,19 +54,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         timerSeekBar = findViewById(R.id.timerSeekBar);
         timerTextView = findViewById(R.id.timerTextView);
-        timerButton = findViewById(R.id.timerButton);
+        timerButton = findViewById(R.id.timerStartButton);
+        mp = MediaPlayer.create(getApplicationContext(), R.raw.alarm);
 
         timerSeekBar.setMax(600);
         timerSeekBar.setProgress(30);
 
         timerTextView.setText("00:30");
 
+        setTimer(30000);
 
         timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 setTimerText(progress * 1000);
-                changeInstanceTimer(progress * 1000);
+                setTimer(progress * 1000);
             }
 
             @Override
@@ -62,16 +85,19 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void changeInstanceTimer(final int milliseconds) {
+    private void setTimer(final int milliseconds) {
         timer = new CountDownTimer(milliseconds, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
+                timeLeft = (int) millisUntilFinished;
                 setTimerText((int) millisUntilFinished);
             }
 
             @Override
             public void onFinish() {
-
+                mp.start();
+                timerButton.setText("stop");
+                timerState = 3;
             }
 
         };
@@ -91,4 +117,6 @@ public class MainActivity extends AppCompatActivity {
         timerTextView.setText(timerText);
 
     }
+
+
 }
